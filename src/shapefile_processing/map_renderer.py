476440f64b1a2b7b3ae7044ccddf71811583eec9
Marin Leontenko <marin.leontenko@ses.com>
2026-03-11 11:04:11 +0100

    refactor

---
+++ src/shapefile_processing/map_renderer.py
@@ -0,0 +1,42 @@
+from PyQt6.QtCore import QPointF
+from PyQt6.QtGui import QBrush, QColor, QPolygonF
+from PyQt6.QtWidgets import QGraphicsPolygonItem
+import pyqtgraph as pg
+
+
+class MapRenderer:
+    def __init__(self, plot_widget):
+        self.plot_widget = plot_widget
+
+    def render_polygons(self, gdf):
+        for geometry in gdf.geometry:
+            if geometry is None or geometry.is_empty:
+                continue
+
+            if geometry.geom_type == 'Polygon':
+                polygons = [geometry]
+            elif geometry.geom_type == 'MultiPolygon':
+                polygons = list(geometry.geoms)
+            else:
+                continue
+
+            for polygon in polygons:
+                x_values, y_values = polygon.exterior.xy
+
+                polygon_item = QPolygonF()
+                for x, y in zip(x_values, y_values):
+                    polygon_item.append(QPointF(x, y))
+
+                graphics_polygon = QGraphicsPolygonItem(polygon_item)
+                graphics_polygon.setPen(pg.mkPen((70, 95, 130, 220), width=0.8))
+                graphics_polygon.setBrush(QBrush(QColor(70, 95, 130, 150)))
+                self.plot_widget.addItem(graphics_polygon)
+
+    def set_plot_range(self, gdf):
+        bounds = gdf.total_bounds
+        min_x, min_y, max_x, max_y = bounds
+        if min_x != max_x and min_y != max_y:
+            self.plot_widget.setXRange(min_x, max_x, padding=0.05)
+            self.plot_widget.setYRange(min_y, max_y, padding=0.05)
+        else:
+            self.plot_widget.enableAutoRange()
--- src/shapefile_processing/shapefile_manager.py
+++ src/shapefile_processing/shapefile_manager.py
@@ -1,13 +1,12 @@
-from PyQt6.QtCore import QPointF
-from PyQt6.QtGui import QBrush, QColor, QPolygonF
-from PyQt6.QtWidgets import QGraphicsPolygonItem
 import geopandas as gpd
-import pyqtgraph as pg
+
+from shapefile_processing.map_renderer import MapRenderer
 
 
 class ShapefileManager:
-    def __init__(self, plot_widget):
+    def __init__(self, plot_widget, map_renderer=None):
         self.plot_widget = plot_widget
+        self.map_renderer = map_renderer or MapRenderer(plot_widget)
         self.loaded_gdf = None
 
     def load_and_render(self, file_name):
@@ -19,42 +18,9 @@ class ShapefileManager:
         if gdf.empty:
             return False
 
-        self.render_polygons(gdf)
-        self.set_plot_range(gdf)
+        self.map_renderer.render_polygons(gdf)
+        self.map_renderer.set_plot_range(gdf)
         return True
-
-    def render_polygons(self, gdf):
-        for geometry in gdf.geometry:
-            if geometry is None or geometry.is_empty:
-                continue
-
-            if geometry.geom_type == 'Polygon':
-                polygons = [geometry]
-            elif geometry.geom_type == 'MultiPolygon':
-                polygons = list(geometry.geoms)
-            else:
-                continue
-
-            for polygon in polygons:
-                x_values, y_values = polygon.exterior.xy
-
-                polygon_item = QPolygonF()
-                for x, y in zip(x_values, y_values):
-                    polygon_item.append(QPointF(x, y))
-
-                graphics_polygon = QGraphicsPolygonItem(polygon_item)
-                graphics_polygon.setPen(pg.mkPen((70, 95, 130, 220), width=0.8))
-                graphics_polygon.setBrush(QBrush(QColor(70, 95, 130, 150)))
-                self.plot_widget.addItem(graphics_polygon)
-
-    def set_plot_range(self, gdf):
-        bounds = gdf.total_bounds
-        min_x, min_y, max_x, max_y = bounds
-        if min_x != max_x and min_y != max_y:
-            self.plot_widget.setXRange(min_x, max_x, padding=0.05)
-            self.plot_widget.setYRange(min_y, max_y, padding=0.05)
-        else:
-            self.plot_widget.enableAutoRange()
 
     def get_attributes(self):
         if self.loaded_gdf is None:
